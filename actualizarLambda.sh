8cce171cc6427c545521087f39d9036b4b84bb0c
Monica <monibrave1@gmail.com>
2025-10-16 20:11:18 -0300

    actualizacion retorno json y .sh

--- actualizarLambda.sh
+++ actualizarLambda.sh
@@ -1,9 +1,43 @@
+
 #!/bin/bash
+#Despliegue de back en lambdapor sript .sh
 
-# Colores básicos
-GREEN="\e[32m"
-YELLOW="\e[33m"
-RESET="\e[0m"
+REPO_URL="https://github.com/monick96/aws-lambda-node-express-prueba"
+REPO_DIR="aws-lambda-node-express-prueba"
+ZIP_NAME="lambdaHuntech.zip"
+LAMBDA_FUNCTION_NAME="lambda-express-prueba"
 
-echo -e "${GREEN} Script de despliegue Lambda iniciado ${RESET}"
+echo "Preparando despliegue Lambda..."
 
+# Verificar si el repositorio ya existe, si existe un pull
+if [ -d "$REPO_DIR" ]; then
+  echo "Repositorio ya existe. Haciendo pull..."
+  cd "$REPO_DIR" || exit
+  git pull
+else
+  echo "Clonando repositorio..."
+  git clone "$REPO_URL"
+  cd "$REPO_DIR" || exit
+fi
+
+echo "Instalando dependencias..."
+npm install
+
+echo "Creando archivo ZIP..."
+zip -r "$ZIP_NAME" . 
+
+echo "Subiendo código a Lambda..."
+aws lambda update-function-code \
+  --function-name "$LAMBDA_FUNCTION_NAME" \
+  --zip-file "fileb://$ZIP_NAME"
+
+echo "Despliegue completado!"
+
+# Volver al directorio padre
+cd ..
+
+# Eliminar carpeta del cli de aws
+echo "Eliminando carpeta del repositorio..."
+rm -rf "$REPO_DIR"
+
+echo "Script finalizado."
--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const serverless = require('serverless-http');//el adaptador de expreess para la
 const express = require('express');
 const app = express();
 
+//const HOSTNAME = '127.0.0.1';
+//const PORT = 3000;
+
 app.get('/', (req, res) => {
-    res.send('<h1>Hola GRUPO 6 desde Express + serverless-http en AWS LAMBDA 👋</h1>');
+    res.status(200);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ mensaje: 'Hola GRUPO 6 desde Express + serverless-http en AWS LAMBDA 👋'}));
 });
+
+//inicia server y escucha solicitudes
+//3 parametros=> puerto, hostname, callback
+//app.listen(PORT,HOSTNAME, () =>console.log(`El servidor esta corriendo en http://${HOSTNAME}:${PORT}`));
 
 //exporta la funcion que lambda va a invocar 
 module.exports.handler = serverless(app);
